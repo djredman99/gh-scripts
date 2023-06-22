cae60a98f5d50414a3e13e6b5c913f19ddcf18ff
Daniel Redman <43892709+djredman99@users.noreply.github.com>
2023-06-22 14:33:53 -0400

    Add files via upload

---
+++ powershell/list-projects.ps1
@@ -0,0 +1,22 @@
+﻿$org='djredman99-org'
+
+
+$projects = gh api graphql -F login=$org -f query='
+  query($login: String!) {
+      organization(login: $login) {
+        projectsV2(first: 100) {
+          nodes {
+            id
+            number
+            title
+          }
+        }
+      }
+    }
+'
+
+$p = $projects | ConvertFrom-Json
+$p.data.organization.projectsv2.nodes | foreach {
+    
+    "Project is " + $_.number + ":" + $_.title + $_.id
+}
