3cc324ef95bbca160ca36214f2a1cf6ca9175823
Daniel Redman <43892709+djredman99@users.noreply.github.com>
2023-06-23 22:07:27 -0400

    Add files via upload

---
+++ powershell/list-hooks-secrets-variables.ps1
@@ -0,0 +1,46 @@
+﻿$org='djredman99-org'
+
+# if you need to auth
+# $env:GH_TOKEN = '<add your PAT>'
+# gh auth login
+
+
+$repos = gh api graphql --paginate -F login=$org --jq '.[].organization.repositories.nodes[]' -f query='
+  query($login: String!) {
+      organization(login: $login) {
+        repositories(first: 100) {
+          nodes {
+            name
+          }
+          totalCount
+          pageInfo {
+            hasNextPage
+            endCursor
+            startCursor
+            hasPreviousPage
+          }
+        }
+      }
+    }
+'
+
+$r = $repos | ConvertFrom-Json
+$r | foreach {  
+   
+    $h = gh api repos/$($org)/$($_.name)/hooks
+    $webhooks = $h | ConvertFrom-Json
+    $wh_count = $webhooks | measure
+    "Repo " + $_.name + " has:"
+    "    " + $wh_count.Count + " webhook(s)"
+
+    $s = gh api repos/$($org)/$($_.name)/actions/secrets
+    $secrets = $s | ConvertFrom-Json
+    $secret_count = $secrets | measure
+    "    " + $secret_count.Count + " secret(s)"
+
+    $v = gh api repos/$($org)/$($_.name)/actions/variables
+    $variables = $v | ConvertFrom-Json
+    $variable_count = $variables | measure
+    "    " + $variable_count.Count + " variable(s)"
+}
+
