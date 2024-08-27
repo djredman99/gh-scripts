ddc1f7feeb660d86a23b043b844464125a7c85ba
djredman99 <djredman99@github.com>
2024-08-26 22:11:39 -0400

    adding a few more commands and comments

--- git-filter-repo/commands.sh
+++ git-filter-repo/commands.sh
@@ -1,4 +1,13 @@
+#!bin/bash
+
 python3 ../git-filter-repo/git-filter-repo --analyze
 
+# Keep only the specified path, removes everything else
+python3 ../git-filter-repo/git-filter-repo --path <path to keep>
+
+# You can specify more than one path to keep, removes everything else
+python3 ../git-filter-repo/git-filter-repo --path <path to keep> --path <second-path-to-keep> --path <third-path-to-keep>
+
+# Deletes the specified folder, must have the --invert-paths argument or it would delete everything but the specified path 
 python3 ../git-filter-repo/git-filter-repo --path <folder to delete> --invert-paths
 
