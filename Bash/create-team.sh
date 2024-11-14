e8d030213cfc51323e65106a01214385071dac5f
Daniel Redman <43892709+djredman99@users.noreply.github.com>
2024-11-14 15:59:46 +0000

    adding idp group

--- Bash/create-team.sh
+++ Bash/create-team.sh
@@ -2,18 +2,19 @@
 
 # Set your token in the GITHUB_TOKEN environment variable
 
-# Function to create a team in the specified organization
+# Function to create a team in the specified organization and link it to an IDP group
 create_team() {
   local org_name=$1
   local team_name=$2
+  local idp_group=$3
   curl -s -X POST -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
-    -d "{\"name\": \"$team_name\"}" \
+    -d "{\"name\": \"$team_name\", \"ldap_dn\": \"$idp_group\"}" \
     "https://api.github.com/orgs/$org_name/teams"
 }
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <org_name> <team_name>"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <org_name> <team_name> <idp_group>"
   exit 1
 fi
 
@@ -26,6 +27,7 @@ fi
 # Assign arguments to variables
 ORG_NAME=$1
 TEAM_NAME=$2
+IDP_GROUP=$3
 
-# Create the team
-create_team $ORG_NAME $TEAM_NAME
+# Create the team and link it to the IDP group
+create_team "$ORG_NAME" "$TEAM_NAME" "$IDP_GROUP"
